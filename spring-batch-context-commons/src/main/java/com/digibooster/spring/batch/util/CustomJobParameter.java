1a27fd629c9395d7cbe3aeb4040413ac0e1ec900
ZAHID Mohammed <zahid.med@gmail.com>
2021-04-10 00:09:19 +0100

    Add comments

--- spring-batch-context-commons/src/main/java/com/digibooster/spring/batch/util/CustomJobParameter.java
+++ spring-batch-context-commons/src/main/java/com/digibooster/spring/batch/util/CustomJobParameter.java
@@ -5,6 +5,14 @@ import java.util.UUID;
 
 import org.springframework.batch.core.JobParameter;
 
+/**
+ * Customized generic class that inherits {@link JobParameter}in order to bypass the restriction made
+ * by spring batch for parameter types (String, Date, Double,Long).
+ * This class was copied from StackOverflow
+ * 
+ * <a href="https://stackoverflow.com/questions/33761730/how-to-send-a-custom-object-as-job-parameter-in-spring-batch">Question link</a>
+ *
+ */
 public class CustomJobParameter<T extends Serializable> extends JobParameter {
 
 	private static final long serialVersionUID = -5137416276827316303L;
